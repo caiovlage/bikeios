d448f161ed8bd21ed0d73f8208f0f586ea986c6a
caiovlage <caiomartinslage@gmail.com>
2019-08-23 22:05:05 -0300

    Initial Commit

---
+++ bikeiosUITests/bikeiosUITests.swift
@@ -0,0 +1,34 @@
+//
+//  bikeiosUITests.swift
+//  bikeiosUITests
+//
+//  Created by caio victor lage on 23/08/19.
+//  Copyright © 2019 caio victor lage. All rights reserved.
+//
+
+import XCTest
+
+class bikeiosUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
