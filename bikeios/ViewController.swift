395b8e4b710389e2de5d1d415e627adb4fd979de
caiovlage <caiomartinslage@gmail.com>
2019-08-24 12:28:54 -0300

    primeiro commit

--- bikeios/ViewController.swift
+++ bikeios/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , CLLocationManagerDelegate{
 
+    
+    var locationManager = CLLocationManager()
+    lazy var mapView = GMSMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view.
     }
 
 
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+       mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        mapView.isMyLocationEnabled = true
+        // Creates a marker in the center of the map.
+       // let marker = GMSMarker()
+        //marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        //marker.title = "Sydney"
+        //marker.snippet = "Australia"
+        //marker.map = mapView
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations.last
+        let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+        
+        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
+                                              longitude: userLocation!.coordinate.longitude, zoom: 13.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        self.view = mapView
+        
+        locationManager.stopUpdatingLocation()
+    }
 }
-
